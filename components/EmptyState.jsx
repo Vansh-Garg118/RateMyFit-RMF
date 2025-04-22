9e9c883406c80dc78b5f30c7ed6cd8ca77f2cb3d
Vansh Garg <vanshgarg25804@gmail.com>
2025-04-22 19:22:31 +0530

    Update the Empty State logic

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -4,6 +4,23 @@ import { images } from "../constants";
 import CustomButton from "./CustomButton";
 import { router } from "expo-router";
 
+// const EmptyState = ({ title, subtitle }) => {
+//   return (
+//     <View className="justify-center items-center px-4">
+//       <Image
+//         source={images.empty}
+//         className="w-[270px] h-[215px]"
+//         resizeMode="contain"
+//       />
+//       <Text className="text-2xl font-psemibold text-white">{title}</Text>
+//       <Text className="font-pmedium text-sm text-gray-100 ">{subtitle}</Text>
+//       <CustomButton title="Create Post" 
+//       handlePress={()=>router.push('/create')} containerStyles='w-full my-5'/>
+//     </View>
+//   );
+// };
+
+
 const EmptyState = ({ title, subtitle }) => {
   return (
     <View className="justify-center items-center px-4">
@@ -19,5 +36,6 @@ const EmptyState = ({ title, subtitle }) => {
     </View>
   );
 };
+
 
 export default EmptyState;
