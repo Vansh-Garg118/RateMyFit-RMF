1f0aea06f07fc5f145fb79eae5870d3d7f990656
VaibhavNegi01 <134778420+VaibhavNegi01@users.noreply.github.com>
2025-04-22 19:24:23 +0530

    Update EmptyState.jsx
    improve code structure

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -4,21 +4,7 @@ import { images } from "../constants";
 import CustomButton from "./CustomButton";
 import { router } from "expo-router";
 
-// const EmptyState = ({ title, subtitle }) => {
-//   return (
-//     <View className="justify-center items-center px-4">
-//       <Image
-//         source={images.empty}
-//         className="w-[270px] h-[215px]"
-//         resizeMode="contain"
-//       />
-//       <Text className="text-2xl font-psemibold text-white">{title}</Text>
-//       <Text className="font-pmedium text-sm text-gray-100 ">{subtitle}</Text>
-//       <CustomButton title="Create Post" 
-//       handlePress={()=>router.push('/create')} containerStyles='w-full my-5'/>
-//     </View>
-//   );
-// };
+
 
 
 const EmptyState = ({ title, subtitle }) => {
